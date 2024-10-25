e39bd83f7141c68e26a38724e394c33b58d21ee0
kmanasagithub <136953759+kmanasagithub@users.noreply.github.com>
2024-10-25 18:33:07 +0530

    Delete

--- Scalarhub-docs-api/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Redirect } from '@docusaurus/router';
-// import  Link  from '@docusaurus/Link';
-           
-
-// Default export for Home component
-export default function Home(): JSX.Element {
-  return <Redirect to="/guides/Overview" />;
-}
